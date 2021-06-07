9e4e51c54263cd8c20ae88fb94a68207b0d18f32
Kresna Satya <satyakresna6295@gmail.com>
2021-06-08 06:42:50 +0800

    follow vercel general folder structure.

--- api/index.js
+++ api/index.js
@@ -5,12 +5,12 @@ module.exports = async (req, res) => {
     try {
         let result = [];
         let year = (new Date()).getFullYear();
-        let text = await fsPromises.readFile(join(__dirname, '..', '..', 'data', `${year}.json`), 'utf8');
+        let text = await fsPromises.readFile(join(__dirname, '..', 'data', `${year}.json`), 'utf8');
 
         let month = req.query.month;
         if (req.query.year) {
             year = req.query.year;
-            text = await fsPromises.readFile(join(__dirname, '..', '..', 'data', `${year}.json`), 'utf8');
+            text = await fsPromises.readFile(join(__dirname, '..', 'data', `${year}.json`), 'utf8');
         }
         let parseResult = JSON.parse(text);
 
